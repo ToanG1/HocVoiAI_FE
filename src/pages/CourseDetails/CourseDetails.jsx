d5d0cbed3ffd5af743da17d749f2d380d1a40256
@ToanG1 <toandinh01675@gmail.com>
2023-09-06 23:08:06 +0700

    course detail page and test text to image api

--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import styles from "./CourseDetails.scss";
 import { useParams } from "react-router";
 
-import SCourseBox from "../../components/SCourseBox/SCourseBox";
-
+import CourseOverview from "../../components/CourseOverview/CourseOverview";
+import CourseCirculum from "../../components/CourseCirculum/CourseCirculum";
 function removeActiveClass() {
   document
     .getElementById("overview-btn")
@@ -12,76 +12,28 @@ function removeActiveClass() {
     .getElementById("curriculum-btn")
     .classList.remove("course-details-active-btn");
 }
-function printOverview() {
-  return (
-    <>
-      <div className="course-details-description">
-        <div>
-          <h1>Course Description</h1>
-          <span>
-            Maximus Ligula Eleifend Id Nisl Quis Interdum. Sed Malesuada Tortor
-            Non Turpis Semper Bibendum. Ut Ac Nisi Porta, Malesuada Risus Non
-            Viverra Dolor. Vestibulum Ante Ipsum Primis In Faucibus Orci Luctus
-            Et Ultrices Posuere Cubilia Curae In Tristique Libero, Quis Ultrices
-            Diam Praesent Varius Diam Dui. Class Aptent Taciti Sociosqu Ad
-            Litora Torquent Per Conubia Nostra. Maximus Ligula Eleifend Id Nisl
-            Quis Interdum. Sed Malesuada Tortor Non Turpis Semper Bibendum. Ut
-            Ac Nisi Porta, Malesuada Risus Non Viverra Dolor. Vestibulum Ante
-            Ipsum Primis In Faucibus Orci Luctus Et Ultrices Posuere Cubilia
-            Curae In Tristique Libero, Quis Ultrices Diam Praesent Varius Diam
-            Dui. Class Aptent Taciti Sociosqu Ad Litora Torquent Per Conubia
-            Nostra.
-          </span>
-        </div>
-        <div>
-          <h1>What Will You Learn From This Course?</h1>
-          <span>
-            Maximus Ligula Eleifend Id Nisl Quis Interdum. Sed Malesuada Tortor
-            Non Turpis Semper Bibendum. Ut Ac Nisi Porta, Malesuada Risus Non
-            Viverra Dolor. Vestibulum Ante Ipsum Primis In Faucibus Orci Luctus
-            Et Ultrices Posuere.
-          </span>
-          <div className="course-details-benefits">
-            <ul>
-              <li>Becom smarter</li>
-              <li>Becom harder</li>
-              <li>Becom better</li>
-            </ul>
-            <ul>
-              <li>Get a job</li>
-              <li>Get respect</li>
-              <li>Get more money</li>
-            </ul>
-          </div>
-        </div>
-        <div className="course-details-relative-courses-header">
-          <span>10,000+ Courses Recommended by AI</span>
-          <h1>You May Also Like More Courses</h1>
-        </div>
-        <div className="course-details-relative-courses">
-          {[1, 2, 3, 4, 5].map((item) => {
-            return (
-              <>
-                <div>
-                  <SCourseBox key={item} />
-                </div>
-              </>
-            );
-          })}
-        </div>
-      </div>
-    </>
-  );
-}
-function printCurriculum() {
-  return (
-    <>
-      <div className="course-details-curriculum">Lessons</div>
-    </>
-  );
-}
-
 export default function CourseDetails() {
+  const [img, setImg] = useState("");
+  useEffect(() => {
+    async function query(data) {
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+        {
+          headers: {
+            Authorization: "Bearer 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",
+          },
+          method: "POST",
+          body: JSON.stringify(data),
+        }
+      );
+      const result = await response.blob();
+      return result;
+    }
+    query({ inputs: "horse running on beach" }).then((response) => {
+      const imageObjectURL = URL.createObjectURL(response);
+      setImg(imageObjectURL);
+    });
+  }, []);
   const param = useParams("courseId");
   const [page, setPage] = useState(0);
   useEffect(() => {
@@ -132,7 +84,7 @@ export default function CourseDetails() {
             </button>
           </div>
           <div className="course-details-body">
-            {page === 0 ? printOverview() : printCurriculum()}
+            {page === 0 ? <CourseOverview /> : <CourseCirculum />}
           </div>
         </div>
         <div className="course-details-sidebar">
@@ -142,7 +94,7 @@ export default function CourseDetails() {
           </div>
           <div className="course-details-sidebar-content">
             <img
-              src="https://picsum.photos/200/300"
+              src={img}
               className="course-details-sidebar-image"
               alt="course-img"
             />

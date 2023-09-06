8e0c66f5ef2a336f7ea2dc1132a89ab853acccd4
@ToanG1 <toandinh01675@gmail.com>
2023-09-06 17:05:47 +0700

    course detail overview

--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import styles from "./CourseDetails.scss";
 import { useParams } from "react-router";
 
+import SCourseBox from "../../components/SCourseBox/SCourseBox";
+
 function removeActiveClass() {
   document
     .getElementById("overview-btn")
@@ -9,6 +11,74 @@ function removeActiveClass() {
   document
     .getElementById("curriculum-btn")
     .classList.remove("course-details-active-btn");
+}
+function printOverview() {
+  return (
+    <>
+      <div className="course-details-description">
+        <div>
+          <h1>Course Description</h1>
+          <span>
+            Maximus Ligula Eleifend Id Nisl Quis Interdum. Sed Malesuada Tortor
+            Non Turpis Semper Bibendum. Ut Ac Nisi Porta, Malesuada Risus Non
+            Viverra Dolor. Vestibulum Ante Ipsum Primis In Faucibus Orci Luctus
+            Et Ultrices Posuere Cubilia Curae In Tristique Libero, Quis Ultrices
+            Diam Praesent Varius Diam Dui. Class Aptent Taciti Sociosqu Ad
+            Litora Torquent Per Conubia Nostra. Maximus Ligula Eleifend Id Nisl
+            Quis Interdum. Sed Malesuada Tortor Non Turpis Semper Bibendum. Ut
+            Ac Nisi Porta, Malesuada Risus Non Viverra Dolor. Vestibulum Ante
+            Ipsum Primis In Faucibus Orci Luctus Et Ultrices Posuere Cubilia
+            Curae In Tristique Libero, Quis Ultrices Diam Praesent Varius Diam
+            Dui. Class Aptent Taciti Sociosqu Ad Litora Torquent Per Conubia
+            Nostra.
+          </span>
+        </div>
+        <div>
+          <h1>What Will You Learn From This Course?</h1>
+          <span>
+            Maximus Ligula Eleifend Id Nisl Quis Interdum. Sed Malesuada Tortor
+            Non Turpis Semper Bibendum. Ut Ac Nisi Porta, Malesuada Risus Non
+            Viverra Dolor. Vestibulum Ante Ipsum Primis In Faucibus Orci Luctus
+            Et Ultrices Posuere.
+          </span>
+          <div className="course-details-benefits">
+            <ul>
+              <li>Becom smarter</li>
+              <li>Becom harder</li>
+              <li>Becom better</li>
+            </ul>
+            <ul>
+              <li>Get a job</li>
+              <li>Get respect</li>
+              <li>Get more money</li>
+            </ul>
+          </div>
+        </div>
+        <div className="course-details-relative-courses-header">
+          <span>10,000+ Courses Recommended by AI</span>
+          <h1>You May Also Like More Courses</h1>
+        </div>
+        <div className="course-details-relative-courses">
+          {[1, 2, 3, 4, 5].map((item) => {
+            return (
+              <>
+                <div>
+                  <SCourseBox key={item} />
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+function printCurriculum() {
+  return (
+    <>
+      <div className="course-details-curriculum">Lessons</div>
+    </>
+  );
 }
 
 export default function CourseDetails() {
@@ -62,11 +132,7 @@ export default function CourseDetails() {
             </button>
           </div>
           <div className="course-details-body">
-            {page === 0 ? (
-              <div className="course-details-description">Description</div>
-            ) : (
-              <div className="course-details-curriculum">Lessons</div>
-            )}
+            {page === 0 ? printOverview() : printCurriculum()}
           </div>
         </div>
         <div className="course-details-sidebar">
